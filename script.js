569c4e2d88637adfa3623bb4747b237503e8065b
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-04-07 10:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Your Script here.
+var newString = "";
+function rot13(s){
+	for (let i = 0; i < s.length; i++) {
+		var char = s[i] + 13;
+		newString += char + 13;
+	}
+	return newString;
+}
+module.exports = rot13;
